473d8676377fb8d222b2159b82c45ca914547775
Peter Darvasi <pdarvasi@hortonworks.com>
2022-04-07 20:58:42 +0200

    CB-13519 - DL-syncer: use CM version from Salt
    This commit introduces the following changes:
    - CB-13519 - CM Server and Agent are fetched via Salt instead of from CM Server
      - to include build numbers amongst the versions, too
    - CB-16986 - validation for different combinations of CM versions received from Salt
      - in case of validation error, the roll-forward is not permitted, until the error is fixed

---
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/cluster/PackageName.java
@@ -0,0 +1,23 @@
+package com.sequenceiq.cloudbreak.service.cluster;
+
+public class PackageName {
+    private String name;
+
+    private String pattern;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPattern() {
+        return pattern;
+    }
+
+    public void setPattern(String pattern) {
+        this.pattern = pattern;
+    }
+}
